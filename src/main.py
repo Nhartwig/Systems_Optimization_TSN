e6b635b400c865ca8e8a8196314463c7d1d574ee
BasianLesi <lroni10094@gmail.com>
2020-11-08 14:57:58 +0100

    Added extended TC3_extended.xml

--- src/main.py
+++ src/main.py
@@ -59,6 +59,7 @@ def printSolution(tsn):
 
 
 filename = '../test_cases/TC3_medium.xml'
+# filename = '../test_cases/TC3_extended.xml'
 # filename = '../test_cases/TC1_check_red.xml'
 # filename = '../test_cases/TC0_example.xml'
 
