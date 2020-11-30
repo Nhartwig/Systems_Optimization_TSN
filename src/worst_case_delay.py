aa1d7b13fb60749c36bb85b5bb7b54c426fb2e5e
Basian Lesi <32136910+BasianLesi@users.noreply.github.com>
2020-11-30 13:38:45 +0100

    Update worst_case_delay.py

--- src/worst_case_delay.py
+++ src/worst_case_delay.py
@@ -1,37 +1,20 @@
 def worst_case_delay(tsn):
-    # print("worst_case_delay")
-    add_priority_to_streams(tsn)            # Assign priorities to streams depending on deadline
     add_stream_to_switch_device(tsn)        # Add streams to every switch
     for device in tsn.devices:
         cycle = 0
         for s in device.egressPort:
-            cycle += s.size/device.speed
+            cycle += s.stream_bandwidth/device.speed
         device.cycleTime = cycle
-        # print(" \033[0m", device.name," cycle time = ", device.cycleTime)
 
-
-def add_priority_to_streams(tsn):
-    # print("add_priority_to_streams")
-    tsn.streams.sort(key=lambda x: x.deadline)
-    for i in range(len(tsn.streams)):
-        tsn.streams[i].priority = 8 - int(i*8/len(tsn.streams))   #divide the streams into 8 equal groups and assing priority based on deadline
-        # print(tsn.streams[i].id," priority = ", tsn.streams[i].priority, " deadline = ", tsn.streams[i].deadline)
 
 
 def add_stream_to_switch_device(tsn):
-    # print("add_stream_to_switch_device")
     for s in tsn.streams:
         s.solution_Links(tsn)
         for l in s.solution_links:
             if l.src.type == "Switch":
                 l.src.egressPort.append(s)
-                # print(s.id, "uses link with source device = ", l.src.name)
 
     for d in tsn.devices:
-        d.egressPort = list(dict.fromkeys(d.egressPort))
+        d.egressPort = list(dict.fromkeys(d.egressPort)) 
 
-    # for d in tsn.devices:
-    #     if d.type == "Switch":
-    #         print("\033[93m", d.name)
-    #         for stream in d.egressPort:
-    #             print("\033[96m", stream.id)
